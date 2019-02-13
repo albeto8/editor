898da9a8fde0381bd9a1612ec04527d23ac7c49c
Mario <albeto8@gmail.com>
2019-02-13 10:21:11 -0600

    add split pane

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import SplitPane from 'react-split-pane';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <SplitPane split="vertical" defaultSize="50%">
+        <div className="editor-pane">
+        </div>
+        <div className="view-pane">
+        </div>
+      </SplitPane>
+    </div>
     );
   }
 }
